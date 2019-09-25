8858d2a9b211dac4e53e4459122ee90388f9c0df
MondaleFelix <mondalefelix@yahoo.com>
2019-09-25 12:15:46 -0700

    added assets andd launch pic

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -8,8 +8,13 @@
 
 import SpriteKit
 
+
+// GamneScene is a subclass of SKScene
+// SpriteKit calls the method didMove before it presents your scene
+// in a view; it's a good place to do some initial setup of your scene's contents
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
     }
 }
+
