dd19a1abc090dda36b1e9932fe0f706d50381589
MondaleFelix <mondalefelix@yahoo.com>
2019-09-25 11:04:29 -0700

    initial commit

---
+++ ZombieConga/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by Mondale on 9/25/19.
+//  Copyright © 2019 Mondale. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+    }
+}
